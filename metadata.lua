60f6b5920ce2b32dd1c9efc30d4bdca99ee67eca
MyOneGod <MyOneGod@proton.me>
2024-07-01 15:54:50 +0200

    initial commit

---
+++ metadata.lua
@@ -0,0 +1,10 @@
+return PlaceObj('ModDef', {
+	'title', "[MOG] Borderless",
+	'id', "MOG__Borderless",
+	'author', "MyOneGod",
+	'version', 1,
+	'lua_revision', 233360,
+	'saved_with_revision', 350233,
+	'default_options', {},
+	'saved', 1719807250,
+})
